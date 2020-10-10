c38501b3df718690a0abf55f02f8e40b3e0cae83
irisida <20445648+irisida@users.noreply.github.com>
2020-10-10 23:29:41 +0100

    added ejs version

---
+++ nodeguide/s6ejsVersion/routes/admin.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const rootDir = require('../utils/pathHelper');
+
+const router = express.Router();
+
+/* temporary storage */
+const products = [];
+
+router.get('/add-product', (req, res, next) => {
+	res.render('add-product', {
+		docTitle: 'Add new Product',
+		path: '/admin/add-product',
+		activeProduct: true,
+		productCSS: true,
+		formsCSS: true,
+	});
+});
+
+router.post('/add-product', (req, res, next) => {
+	products.push({ title: req.body.title });
+	res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
---
+++ nodeguide/s6ejsVersion/routes/shop.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+
+const rootDir = require('../utils/pathHelper');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+	const products = adminData.products;
+	res.render('shop', {
+		products: products,
+		docTitle: 'Shop',
+		path: '/',
+		isPopulated: products.length > 0,
+		activeShop: true,
+		productCSS: true,
+	});
+});
+
+module.exports = router;
